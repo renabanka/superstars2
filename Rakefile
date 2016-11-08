659736fd4a2f7f9735d4ef80078435a6152d4c0e
Rena Banka <renabanka@Renas-MacBook-Pro.local>
2016-11-08 15:23:59 -0600

    feeling great

---
+++ Rakefile
@@ -0,0 +1,7 @@
+require 'sinatra/activerecord'
+require 'sinatra/activerecord/rake'
+
+ActiveRecord::Base.establish_connect(
+    :adapter => 'mysql2',
+    :database => 'superstars'
+)
--- controllers/superstar_controller.rb
+++ controllers/superstar_controller.rb
@@ -1,25 +1,74 @@
 class  SuperstarController < ApplicationController
   get '/' do
-    {:message => 'all items soon'}.to_json
+    # {:message => 'all items soon'}.to_json
+    # get all
+    Superstar.all.to_json
   end
 
-  get '/fab' do
-    #teach you do to rule the database
-    #use activerecord
+  get '/:id' do
+    #get by id
+    @id = params[:id]
+    Superstar.find(@id).to_json
+  end
+
+  post '/' do
     #create
-    @christopher = Superstar.new
-    @christopher.talent = 'Socialiate'
-    @christopher.name = 'Christopher McAwesome'
-    @christopher.outfit = 'blue sweater'
-    @christopher.save
-    @christopher.to_json
+    @name = params[:name]
+    @talent = params[:talent]
+    @outfit = params[:outfit]
 
-    #CRUD
+    @model = Superstar.new
+    @model.name = @name
+    @model.talent = @talent
+    @model.outfit = @outfit
+    @model.save
 
-    #delete
+    # place a breakpoint in ruby!
+    # binding.pry
+    # ^ the code stops here
+    @model.to_json
+  end
+
+  patch '/:id' do
     #update
-    #read
+    @id = params[:id]
+    @name = params[:name]
+    @talent = params[:talent]
+    @outfit = params[:outfit]
+    @model = Superstar.find(@id)
+    @model.name = @name
+    @model.talent = @talent
+    @model.outfit = @outfit
+    @model.save
+    @model.to_json
 
+  end
 
+  delete '/:id' do
+    @id = params[:id]
+    @model = Superstar.find(@id)
+    @model.destroy
+    { :message => "Item of id" + @id + "was removed."}.to_json
+    #delete
   end
 end
+
+
+  # get '/fab' do
+  #   #teach you do to rule the database
+  #   #use activerecord
+  #   #create
+  #   @christopher = Superstar.new
+  #   @christopher.talent = 'Socialiate'
+  #   @christopher.name = 'Christopher McAwesome'
+  #   @christopher.outfit = 'blue sweater'
+  #   @christopher.save
+  #   @christopher.to_json
+  #
+  #   #CRUD
+  #
+  #   #delete
+  #   #update
+  #   #read
+
+  # end
